502da6a97f0092b700cfcec7664d7faa4ec43b2e
Gursha Kasaudhan <gurshakasaudhan1812@gmail.com>
2024-07-20 18:22:14 +0530

    jhdj

---
+++ src/main/java/com/youtube/jwt/entity/Holiday.java
@@ -0,0 +1,54 @@
+package com.youtube.jwt.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Holiday {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long holidayId;
+	private String department;
+	private String designation;
+	private String employmentType;
+	 @OneToMany(mappedBy = "holiday", cascade = CascadeType.ALL, orphanRemoval = true)
+	 private List<HoliDayDateOcassion> holiDayDateOcassion = new ArrayList<>();
+	public Long getHolidayId() {
+		return holidayId;
+	}
+	public void setHolidayId(Long holidayId) {
+		this.holidayId = holidayId;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public List<HoliDayDateOcassion> getHoliDayDateOcassion() {
+		return holiDayDateOcassion;
+	}
+	public void setHoliDayDateOcassion(List<HoliDayDateOcassion> holiDayDateOcassion) {
+		this.holiDayDateOcassion = holiDayDateOcassion;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getEmploymentType() {
+		return employmentType;
+	}
+	public void setEmploymentType(String employmentType) {
+		this.employmentType = employmentType;
+	}
+	
+}
